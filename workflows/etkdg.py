8b6e3b4420350c1cc23dc6f1eec0de7b7f28bfe4
Geoff Hutchison <geoff.hutchison@gmail.com>
2018-07-28 17:41:16 -0400

    Add an example of using RDKit ETKDG from SDF format
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

---
+++ workflows/etkdg.py
@@ -0,0 +1,80 @@
+"""
+/******************************************************************************
+  This source file is part of the Avogadro project.
+
+  This source code is released under the New BSD License, (the "License").
+******************************************************************************/
+"""
+
+import argparse
+import json
+import sys
+import os
+from random import randrange
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+# Some globals:
+debug = True
+
+
+def getOptions():
+    userOptions = {}
+
+    userOptions['ff'] = {}
+    userOptions['ff']['type'] = 'stringList'
+    userOptions['ff']['label'] = 'Force field'
+    userOptions['ff']['default'] = 2
+    userOptions['ff']['values'] = ['None', 'MMFF94', 'UFF']
+
+    opts = {'userOptions': userOptions }
+    opts['inputMoleculeFormat'] = 'sdf'
+
+    return opts
+
+
+def generate(opts):
+    m = Chem.MolFromMolBlock(opts['sdf'])
+    AllChem.EmbedMolecule(m,AllChem.ETKDG())
+
+    if opts['ff'] is 'UFF':
+        AllChem.UFFOptimizeMolecule(m)
+    elif opts['ff'] is 'MMFF94':
+        AllChem.MMFF94OptimizeMolecule(m)
+
+    return Chem.MolToMolBlock(m3)
+
+
+def runWorkflow():
+    # Read options from stdin
+    stdinStr = sys.stdin.read()
+
+    # Parse the JSON strings
+    opts = json.loads(stdinStr)
+
+    # Replace this molecule with a new conformer in SDF
+    result = {}
+    result['moleculeFormat'] = 'sdf'
+    result['sdf'] = generate(opts)
+    return result
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser('RDKit ETKDG')
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--print-options', action='store_true')
+    parser.add_argument('--run-workflow', action='store_true')
+    parser.add_argument('--display-name', action='store_true')
+    parser.add_argument('--menu-path', action='store_true')
+    parser.add_argument('--lang', nargs='?', default='en')
+    args = vars(parser.parse_args())
+
+    debug = args['debug']
+
+    if args['display_name']:
+        print("Generate Conformer...")
+    if args['menu_path']:
+        print("&Extensions|RDKit")
+    if args['print_options']:
+        print(json.dumps(getOptions()))
+    elif args['run_workflow']:
+        print(json.dumps(runWorkflow()))
